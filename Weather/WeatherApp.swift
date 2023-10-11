d0e0b32dea121b52226761ab13431c2c321ff832
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2023-10-11 21:08:43 +0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Shafquat Mahbub on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
